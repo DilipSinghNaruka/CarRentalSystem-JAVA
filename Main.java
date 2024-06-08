aa955d98d9a8746ebb5c556070277132f65662d0
Dilip Singh Naruka <dsnaruka995082@gmail.com>
2024-06-08 21:39:08 +0530

    Car, Customer,Rental class is complete and carRentalSystem class define all condition. next i create a menu function

---
+++ Main.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Car{
+	private String carId;
+	private String brand;
+	private String model;
+	private double basePricePerDay;
+
+	public Car(String carId, String brand, String model, double basePricePerDay){
+		this.carId = carId;
+		this.brand = brand;
+		this.model = model;
+		this.basePricePerDay = basePricePerDay;
+		this.isAvailable = true;
+
+	}
+
+	public String getCarId(){
+		return carId;
+	}
+
+	public String getBrand(){
+		return brand;
+	}	
+
+	public String getModel(){
+		return model;
+	}
+
+	public double calculatePrice(int rentalDays){
+		return basePricePerDay*rentalDays;
+	}
+
+	public boolean isAvailable(){
+		return isAvailable;
+	}
+
+	public void rent(){
+		isAvailable = false;
+	}
+
+	public void returnCar(){
+		isAvailable = true;
+	}
+
+}
+
+class Customer{
+	private String customerId;
+	private String name;
+	public Customer(String customerId, String name){
+		this.customerId = customerId;
+		this.name = name;
+	}
+	
+	public String getCustomerId(){
+		return customerId;
+	}
+	
+	public String getName(){
+		return name;
+
+	}
+
+
+}
+
+class Main{
+
+	public static void main(String args[]){
+		System.out.println("JAI SHREE RAM");
+	}
+}
